37008910350eafccedf13c289f3d212a7dcf3f41
PedroDLF <pedrodlafuente@gmail.com>
2019-05-23 21:52:59 +0200

    jwt login Done

---
+++ backend/SpringBootJwtAuthentication/src/main/java/com/padawans/jwt/controller/AuthRestAPIs.java
@@ -0,0 +1,134 @@
+package com.padawans.jwt.controller;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.padawans.jwt.message.request.LoginForm;
+import com.padawans.jwt.message.request.SignUpForm;
+import com.padawans.jwt.message.response.JwtResponse;
+import com.padawans.jwt.model.Role;
+import com.padawans.jwt.model.RoleName;
+import com.padawans.jwt.model.User;
+import com.padawans.jwt.model.UserResponse;
+import com.padawans.jwt.repository.RoleRepository;
+import com.padawans.jwt.repository.UserRepository;
+import com.padawans.jwt.security.jwt.JwtProvider;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestAPIs {
+	private static final Log LOG = LogFactory.getLog(AuthRestAPIs.class);
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+    
+    @Autowired
+    AplicationController controller;
+   
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateJwtToken(authentication);
+        JwtResponse token = new JwtResponse(jwt);
+        User user = controller.getUsersbyName(loginRequest.getUsername()).getBody();
+        UserResponse response = new UserResponse(user.getId(), user.getName(), user.getSurname(), user.getUsername(), user.getPhone(), user.getEmail(), token);
+        return ResponseEntity.ok(response);
+    }
+    
+    
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpForm signUpRequest) {
+    	LOG.info(signUpRequest.toString());
+    	LOG.info("INICIANDO REGISTRO USUARIO---> "+signUpRequest.getName());
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+        	LOG.info("Fail -> Username is already taken!");
+            return new ResponseEntity<>( HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+        	LOG.info("Fail -> Email is already in use!");
+            return new ResponseEntity<>( HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(signUpRequest.getName(),signUpRequest.getSurname(), signUpRequest.getUsername(),
+        		signUpRequest.getPhone(),  signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        strRoles.forEach(role -> {
+        	switch(role) {
+	    		case "admin":
+	    			Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	    			roles.add(adminRole);
+	    			
+	    			break;
+	    		case "pm":
+	            	Role pmRole = roleRepository.findByName(RoleName.ROLE_PM)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	            	roles.add(pmRole);
+	            	
+	    			break;
+	    		default:
+	        		Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	        		roles.add(userRole);        			
+        	}
+        });
+        
+        user.setRoles(roles);
+        LOG.info("Roles");
+        for (Role item : roles) {
+        	LOG.info("Rol--> "+item.toString());
+		}
+        userRepository.save(user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    
+}
---
+++ backend/SpringBootJwtAuthentication/src/main/java/com/padawans/jwt/message/request/UpdateFrom.java
@@ -0,0 +1,72 @@
+package com.padawans.jwt.message.request;
+
+import java.util.Set;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UpdateFrom {
+	
+	  @NotBlank
+	    @Size(min = 3, max = 50)
+	    private String name;
+	    
+	    @NotBlank
+	    @Size(min = 3, max = 50)
+	    private String surname;
+
+	    
+	    
+	    @NotBlank
+	    @Size(min = 3, max = 50)
+	    private String phone;
+
+	    
+	    private Set<String> role;
+
+
+		public String getName() {
+			return name;
+		}
+
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+
+		public String getSurname() {
+			return surname;
+		}
+
+
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+
+
+		public String getPhone() {
+			return phone;
+		}
+
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+
+		public Set<String> getRole() {
+			return role;
+		}
+
+
+		public void setRole(Set<String> role) {
+			this.role = role;
+		}
+
+	
+	    
+
+
+}
