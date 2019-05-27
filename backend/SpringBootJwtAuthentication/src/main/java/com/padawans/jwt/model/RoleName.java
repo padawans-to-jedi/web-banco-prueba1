c8443990db35d2546e06000607f3554dcf8e9db3
PedroDLF <pedrodlafuente@gmail.com>
2019-05-27 18:21:06 +0200

    delete jwt autentication

--- backend/SpringBootJwtAuthentication/src/main/java/com/padawans/jwt/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.padawans.jwt.model;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_PM,
-    ROLE_ADMIN
-}
--- backend/SpringBootJwtAuthentication/src/main/java/com/scs/jwtauthentication/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.scs.jwtauthentication.model;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_PM,
-    ROLE_ADMIN
-}
