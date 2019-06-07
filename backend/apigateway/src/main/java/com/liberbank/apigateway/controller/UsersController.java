43c0dc1e2d6cb4694e78103564c1c5f75e17f39d
pedrodlf <pedrodlafuente@gmail.com>
2019-06-07 23:20:21 +0200

    update

--- backend/apigateway/src/main/java/com/liberbank/apigateway/controller/UsersController.java
+++ backend/apigateway/src/main/java/com/liberbank/apigateway/controller/UsersController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,31 +19,32 @@ import com.example.generated.model.UserDataUpdate;
 import com.liberbank.apigateway.service.UserService;
 
 @Controller
-@CrossOrigin(origins = "*", methods= {RequestMethod.POST,RequestMethod.GET,RequestMethod.PUT})
-public class UsersController implements UsersApi{
+@CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT })
+public class UsersController implements UsersApi {
 
-	@Autowired
-	UserService service;
+    @Autowired
+    UserService service;
 
-	@Override
-	public ResponseEntity<AccountsGetResponse> usersUserIDAccountsGet(@Min(1) Long userID) {
-		return service.usersUserIDAccountsGet(userID);
-	}
+    @Override
+    public ResponseEntity<AccountsGetResponse> usersUserIDAccountsGet(@Min(1) Long userID) {
+        return service.usersUserIDAccountsGet(userID);
+    }
 
-	@Override
-	public ResponseEntity<AccountsResponse> usersUserIDAccountsPost(@Min(1) Long userID,
-			@Valid Accountrequest accountRequest) {
-		return service.usersUserIDAccountsPost(userID, accountRequest);
-	}
+    @Override
+    public ResponseEntity<AccountsResponse> usersUserIDAccountsPost(@Min(1) Long userID,
+            @Valid Accountrequest accountRequest) {
+        return service.usersUserIDAccountsPost(userID, accountRequest);
+    }
 
-	@Override
-	public ResponseEntity<Void> usersUserIDPaymentsPost(@Min(1) Long userID, @Valid PaymentsRequest paymentRequest) {
-		return service.usersUserIDPaymentsPost(userID, paymentRequest);
-	}
+    @Override
+    public ResponseEntity<MessageResponse> usersUserIDPut(@Min(1) Long userID, @Valid UserDataUpdate userdata) {
+        return service.usersUserIDPut(userID, userdata);
+    }
 
-	@Override
-	public ResponseEntity<MessageResponse> usersUserIDPut(@Min(1) Long userID, @Valid UserDataUpdate userdata) {
-		return service.usersUserIDPut(userID, userdata);
-	}
+    @Override
+    public ResponseEntity<Void> usersUserIDPaymentsPost(@Min(1) Long userID, PaymentsRequest paymentRequest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
